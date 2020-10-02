65d5c0a10aff9a04470d496005fcd60eff3d7e8b
Nagarjuna Vatti <46709938+nagarjunavatti@users.noreply.github.com>
2020-10-02 17:57:08 +0530

    Create arduino_code.ino

---
+++ Communication between RPi and Arduino/arduino_code.ino
@@ -0,0 +1,31 @@
+#include <SoftwareSerial.h>
+
+int Serialdata=0;
+void setup() {
+// put your setup code here, to run once:
+pinMode(13,OUTPUT);
+pinMode(11,OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()>0)
+{
+
+  Serialdata=Serial.read();
+  Serial.println(Serialdata);
+
+  if(Serialdata == '0')
+  {
+    digitalWrite(13,HIGH);
+    //digitalWrite(11,LOW);
+
+  }
+  else if(Serialdata == '1')
+  {
+    digitalWrite(13,LOW);
+    //digitalWrite(11,HIGH);
+  }
+}
+}
